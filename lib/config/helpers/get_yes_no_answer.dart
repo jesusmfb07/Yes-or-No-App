60b3e022332207d30947e8f142d664124941878a
Yisus <jesusmfb07@gmail.com>
2024-06-04 11:38:16 -0500

    feat: Correcting stacios

--- lib/config/helpers/get_yes_no_answer.dart
+++ lib/config/helpers/get_yes_no_answer.dart
@@ -4,6 +4,7 @@ import 'package:flutter_application_1/domain/entities/message.dart';
 import 'package:flutter_application_1/infrastructure/models/yes_no_model.dart';
 
 class GetYesNoAnswer {
+  
   final _dio = Dio();
 
   Future<Message> getAnswer() async {
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,9 @@
 import 'package:flutter/material.dart';
-
+import 'package:provider/provider.dart';
 
 import 'package:flutter_application_1/config/theme/app_theme.dart';
 import 'package:flutter_application_1/presentation/providers/chat_provider.dart';
 import 'package:flutter_application_1/presentation/screens/chat/chat_screen.dart';
-import 'package:provider/provider.dart';
 
 void main() => runApp(const MyApp());
 
